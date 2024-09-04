4f3c7d3e47d628557640672413fda35eb9ee7a16
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-09-04 20:23:24 -0300

    Aplicar prittier

---
+++ next-challenges/src/challenges/findChange.js
@@ -0,0 +1,13 @@
+const findNonConstructibleChange = (coins) => {
+  const orderedCoins = coins.sort((a, b) => a - b);
+  let change = 1;
+  for (let i = 0; i < orderedCoins.length; i++) {
+    if (orderedCoins[i] > change) {
+      return change;
+    }
+    change += orderedCoins[i];
+  }
+  return change;
+};
+
+export default findNonConstructibleChange;
