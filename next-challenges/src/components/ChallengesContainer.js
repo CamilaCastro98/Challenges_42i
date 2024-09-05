dfb75db2d360d59b5b2a563cba47389168b0ffcb
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-09-05 11:14:11 -0300

    Ultimos detalles estéticos

--- next-challenges/src/components/ChallengesContainer.js
+++ next-challenges/src/components/ChallengesContainer.js
@@ -4,13 +4,13 @@ import Challenge2 from "./Challenge2";
 const ChallengesContainer = () => {
   return (
     <div className="flex flex-col justify-center gap-8 lg:flex-row">
-    <div className="w-full lg:max-w-lg">
-      <Challenge1 />
+      <div className="w-full lg:max-w-lg">
+        <Challenge1 />
+      </div>
+      <div className="w-full lg:max-w-lg">
+        <Challenge2 />
+      </div>
     </div>
-    <div className="w-full lg:max-w-lg">
-      <Challenge2 />
-    </div>
-  </div>
   );
 };
 
