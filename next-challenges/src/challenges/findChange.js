512739e4db81b0161f7f0c4265fd689069b2463e
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-09-05 15:26:33 -0300

    Agregar comentarios

--- next-challenges/src/challenges/findChange.js
+++ next-challenges/src/challenges/findChange.js
@@ -1,12 +1,31 @@
+//Función que toma un array de valores enteros positivos (coins) y calcula cuál es el
+//menor entero que no se puede obtener de la combinación de los elementos del array.
+//Cuando el array está vacío, se retorna 1.
+
 const findNonConstructibleChange = (coins) => {
+  //Ordenar el arreglo de forma ascendente, de esta forma es más fácil
+  //ir evaluando los enteros que pueden o no formarse.
   const orderedCoins = coins.sort((a, b) => a - b);
+
+  //Definir e inicializar la variable "change" en 1, ya que es el mínimo entero
+  //posible que puede evaluarse.
   let change = 1;
+
+  //Crear un bucle que recorra todos los elementos del array ordenado.
   for (let i = 0; i < orderedCoins.length; i++) {
+    //Evaluar si el elemento actual en el bucle es más grande que el valor acumulado de "change".
     if (orderedCoins[i] > change) {
+      //Si la condición se cumple, quiere decir que no es posible llegar al valor de "change" con
+      //Los elementos existentes. Retornar "change".
       return change;
     }
+    //Si la condición no se cumple, quiere decir que el valor de "change" sí puede alcanzarse con
+    //alguna combinación de los elementos ya evaluados.
+    //Sumar el elemento actualmente evaluado a "change" para continuar evaluando valores enteros mayores.
     change += orderedCoins[i];
   }
+  // Si el bucle se completa sin encontrar un valor de "change" que no pueda ser construido,
+  // retornar el valor de "change", que será el siguiente entero no construible.
   return change;
 };
 
--- next-challenges/src/challenges/findPairWithSum.js
+++ next-challenges/src/challenges/findPairWithSum.js
@@ -1,12 +1,31 @@
+//Función que toma un array de números (numbers) y un valor numérico (targetSum) y confirma si existe
+//un par de elementos en el array que sumen el valor introducido, devolviendo
+//el par o un array vacío dependiendo del caso.
+
 const findPairWithSum = (numbers, targetSum) => {
-  const pastNumbers = new Set()  
+  //Crear una variable que vaya acumulando los valores del array leídos por el posterior bucle.
+  const pastNumbers = new Set();
+
+  //Crear un bucle que recorra cada elemento del array para buscar un par compatible.
   for (let i = 0; i < numbers.length; i++) {
+    //Evaluar si existe un elemento previamente leído por el bucle que, sumado al elemento actualmente evaluado,
+    //sea igual a "targetSum".
     if (pastNumbers.has(targetSum - numbers[i])) {
-      return [targetSum - numbers[i],numbers[i]];
+      //Si existe el elemento, se retorna el par.
+      return [targetSum - numbers[i], numbers[i]];
     }
-    pastNumbers.add(numbers[i])
+
+    //Si no se cumple la condición, el elemento actualmente evaluado se añade a la lista de elementos ya leídos,
+    //Para poder compararlo con el resto de los elementos que quedan por evaluar.
+    pastNumbers.add(numbers[i]);
   }
+
+  //Si el bucle se completa y el código prosigue, significa que el par no se encontró.
+  //Retornar el array vacío.
   return [];
 };
 
 module.exports = findPairWithSum;
+
+//Opté por usar un Set en lugar de un bucle anidado debido a que la complejidad de tiempo del algoritmo
+//es mayor con esta última opción para grandes inputs, por lo que el uso de Set es más eficiente.
